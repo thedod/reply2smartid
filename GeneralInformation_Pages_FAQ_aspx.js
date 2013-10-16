4ed73f63fc36fda97af700d89428d99b3640579a
The Dod <unclezzzen@gmail.com>
2013-10-16 07:35:48 +0700

    Voodoo

--- GeneralInformation_Pages_FAQ_aspx.js
+++ GeneralInformation_Pages_FAQ_aspx.js
@@ -1,4 +1,4 @@
 var truthlinks = [
     ["הכולל אמצעים ונתונים ביומטריים", "bio-id-needs-no-db.html"],
     ["מדי שנה", "long-time-no-id.html"]
-]
+];
--- Pages_default_aspx.js
+++ Pages_default_aspx.js
@@ -1,3 +1,3 @@
 var truthlinks = [
     ["איך מקבלים תעודת זהות חכמה ודרכון חכם?", "how-to-get-smart.html"]
-]
+];
