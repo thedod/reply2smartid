f0b0a3fb454a32b4c5e869d15f8e06d2f7496f27
The Dod <unclezzzen@gmail.com>
2013-10-17 09:07:22 +0700

    Added truthmap

---
+++ truthmap.js
@@ -0,0 +1,13 @@
+// This file should be uploaded to the tomodo faqbuster mod
+// every time it's modidied, but we keep it here (and not at faqbuster)
+// to better separate between site-specific and general-purpose code
+var truthmap = {
+    '/Pages/default.aspx': [
+        ["div.HPInformationTitle","איך מקבלים תעודת זהות חכמה ודרכון חכם?", "how-to-get-smart.html"]
+    ], 
+    '/GeneralInformation/Pages/FAQ.aspx': [
+        ["div.cont","הכולל אמצעים ונתונים ביומטריים", "bio-id-needs-no-db.html"],
+        ["div.cont","מדי שנה", "long-time-no-id.html"],
+        ["div.cont","ובמאגר מידע","law-name-says-db.html"],
+    ]   
+}
